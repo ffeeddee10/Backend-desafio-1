4b5e85f30e096b30a06f03585715120a7892aaac
federico gimenez <ffeeddee_10@hotmail.com>
2023-05-16 21:53:30 -0300

    desafio 2

--- desafio1.js
+++ desafio1.js
@@ -4,8 +4,15 @@
 // 4 creo los productos o eventos deseados (addevents) y dentro de esa funcion creo los productos deseados ( nombre, artistas, fecha, etc)
 // 5 instancia el evento, lo hace correr, lo hace funcionar
 // 6 creo la cantidad de productos deseados con toda la informacion
-// 7 lleno el array vacio creado de (events) del punto 2, se hace con (this.#events.push) pusheo todo el evento y creo los objetos dentro de ese evento 
+// 7 lleno el array vacio creado de (events) del punto 2, se hace con (this.#events.push) pusheo todo el evento y creo los objetos dentro de ese evento
 // 8 genero un id automatico
+//desafio 2
+// 9 creo un packjson con npm init -y , y en json escribir "type": "module",
+// 10 importo ese module
+// 11 dentro de el array vacio lo hago sincronico y creo un Json mediante promesas, mejoro agregando en strngify ( null, '\t') lo apila
+
+//10
+import fs from 'fs'
 
 // 1 //
 class Tortas {
@@ -39,14 +46,19 @@ class Tortas {
         }
     }
     // 4 //
-    addEvent = (name, relleno, saborDelBiscochuelo, price, code, kilogramos, img) => {
+    addEvent = async (name, relleno, saborDelBiscochuelo, price, code, kilogramos, img) => {
         this.#validateEvent(name, relleno, saborDelBiscochuelo, price, code, kilogramos,img)
         if (this.#error === undefined)
             // 7 //
             this.#events.push({ id: this.#generateId(), name, relleno, saborDelBiscochuelo, price, code, kilogramos, img })
         else
             console.log(this.#error)
+
+        // 11
+    await fs.promises.writeFile('./tortas.json', JSON.stringify(this.#events, null, '\t'))
+        
     }
+   
 }
 
 // 5 //
@@ -56,3 +68,5 @@ tortasConProductos.addEvent('torta Minions', 'dulce de leche y oreo', 'vainilla'
 tortasConProductos.addEvent('torta kakita', 'dulce de leche con fruta a eleccion', 'Chocolate', 2000, '102', 4, 'imagen2')
 tortasConProductos.addEvent('torta carousel', 'mouse de chocolate', ' chocolate y vainilla', 4000, 3, '103', 'imagen3') 
 tortasConProductos.addEvent('torta casamiento', 'dulce de leche y chocolate', 'sin sabor', 20000, 15, '104', 'imagen4')
+
+
